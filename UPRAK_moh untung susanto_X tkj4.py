bf4834c0d24a0e5e678e7eb4b9f50b1b23da57cd
santooooooooo <112526718+santooooooooo@users.noreply.github.com>
2022-09-10 05:46:07 -0700

    Add files via upload

---
+++ UPRAK_moh untung susanto_X tkj4.py
@@ -0,0 +1,37 @@
+# rumus volume balok panjang * lebar *tinggi 
+
+panjang = 5
+lebar = 10
+tinggi = 15
+volume_b =panjang * lebar *tinggi
+print("volume kubus adalah", volume_b)
+
+# rumus volume kubus  sisi * sisi * sisi
+
+sisi = 5
+sisi = 5
+sisi = 5
+volume_k = sisi * sisi * sisi 
+print("volume kubus adalah", volume_k)
+
+# rumus volume limas 1/3 * alas * tinggi
+
+alas = 5
+tinggi = 6
+volume_1 = alas * tinggi /3
+print("volume limas adaalah",volume_1)
+
+# rumus volume tabung 22/7 * r * r * t
+
+r = 7
+r = 7
+t = 10
+volume_t = r* r * t * 22/7
+print("volume limas adalah", volume_t)
+
+# celcius ke reamor celcius * 4/5
+
+celcius = 40
+reamor = 4/5
+suhu = 40 * 4/5
+print("celcius dijadikan ke reamor",suhu)
